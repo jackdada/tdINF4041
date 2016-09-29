5ae6775d1b6c10f1d36cde2437c0f1b8ffb80321
RONDOT Romain <ETD-P\rondot@i22-16-lx.etd-p.esiea.fr>
2016-09-29 10:34:47 +0200

    version 3

---
+++ app/models/brasserie.rb
@@ -0,0 +1,3 @@
+class Brasserie < ActiveRecord::Base
+	has_many :beers
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915103901) do
+ActiveRecord::Schema.define(version: 20160929071838) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name"
@@ -23,5 +23,12 @@ ActiveRecord::Schema.define(version: 20160915103901) do
   end
 
   add_index "beers", ["brasserie_id"], name: "index_beers_on_brasserie_id"
+
+  create_table "brasseries", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
